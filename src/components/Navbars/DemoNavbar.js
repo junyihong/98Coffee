6d72f86150c3fc9e49c762f1a3fffd9c9f4a0ee4
junyihong <junyihong@naver.com>
2023-06-07 09:56:07 +0900

    First Commit

---
+++ src/components/Navbars/DemoNavbar.js
@@ -0,0 +1,188 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// JavaScript plugin that hides or shows a component based on your scroll
+import Headroom from "headroom.js";
+// reactstrap components
+import {
+  Button,
+  UncontrolledCollapse,
+  DropdownMenu,
+  DropdownItem,
+  DropdownToggle,
+  UncontrolledDropdown,
+  Media,
+  NavbarBrand,
+  Navbar,
+  NavItem,
+  NavLink,
+  Nav,
+  Container,
+  Row,
+  Col,
+  UncontrolledTooltip,
+} from "reactstrap";
+
+class DemoNavbar extends React.Component {
+  componentDidMount() {
+    let headroom = new Headroom(document.getElementById("navbar-main"));
+    // initialise
+    headroom.init();
+  }
+  state = {
+    collapseClasses: "",
+    collapseOpen: false,
+  };
+
+  onExiting = () => {
+    this.setState({
+      collapseClasses: "collapsing-out",
+    });
+  };
+
+  onExited = () => {
+    this.setState({
+      collapseClasses: "",
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <header className="header-global">
+          <Navbar
+            className="navbar-main navbar-transparent navbar-light headroom"
+            expand="lg"
+            id="navbar-main"
+          >
+            <Container>
+              <NavbarBrand className="mr-lg-5" to="/" tag={Link}>
+                <img
+                  alt="..."
+                  src={require("assets/img/brand/98logo.png")}
+                />
+              </NavbarBrand>
+              <button className="navbar-toggler" id="navbar_global">
+                <span className="navbar-toggler-icon" />
+              </button>
+              <UncontrolledCollapse
+                toggler="#navbar_global"
+                navbar
+                className={this.state.collapseClasses}
+                onExiting={this.onExiting}
+                onExited={this.onExited}
+              >
+                <div className="navbar-collapse-header">
+                  <Row>
+                    <Col className="collapse-brand" xs="6">
+                      <Link to="/">
+                        <img
+                          alt="..."
+                          src={require("assets/img/brand/98logo2.png")}
+                        />
+                      </Link>
+                    </Col>
+                    <Col className="collapse-close" xs="6">
+                      <button className="navbar-toggler" id="navbar_global">
+                        <span />
+                        <span />
+                      </button>
+                    </Col>
+                  </Row>
+                </div>
+                <Nav className="navbar-nav-hover align-items-lg-center" navbar>
+                  <UncontrolledDropdown nav>
+                    <DropdownToggle nav>
+                      <i className="ni ni-collection d-lg-none mr-1" />
+                      <span className="nav-link-inner--text">둘러보기</span>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem to="/landing-page" tag={Link}>
+                        홈
+                      </DropdownItem>
+                      <DropdownItem to="/profile-page" tag={Link}>
+                        Profile
+                      </DropdownItem>
+                      <DropdownItem to="/login-page" tag={Link}>
+                        Login
+                      </DropdownItem>
+                      <DropdownItem to="/register-page" tag={Link}>
+                        Register
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+                <Nav className="align-items-lg-center ml-lg-auto" navbar>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.facebook.com/creativetim"
+                      id="tooltip333589074"
+                      target="_blank"
+                    >
+                      <i className="fa fa-facebook-square" />
+                      <span className="nav-link-inner--text d-lg-none ml-2">
+                        페이스북
+                      </span>
+                    </NavLink>
+                    <UncontrolledTooltip delay={0} target="tooltip333589074">
+                    페이스북
+                    </UncontrolledTooltip>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://www.instagram.com/98_coffee_/"
+                      id="tooltip356693867"
+                      target="_blank"
+                    >
+                      <i className="fa fa-instagram" />
+                      <span className="nav-link-inner--text d-lg-none ml-2">
+                        인스타그램
+                      </span>
+                    </NavLink>
+                    <UncontrolledTooltip delay={0} target="tooltip356693867">
+                      인스타그램
+                    </UncontrolledTooltip>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink
+                      className="nav-link-icon"
+                      href="https://twitter.com/creativetim"
+                      id="tooltip184698705"
+                      target="_blank"
+                    >
+                      <i className="fa fa-twitter-square" />
+                      <span className="nav-link-inner--text d-lg-none ml-2">
+                        트위터
+                      </span>
+                    </NavLink>
+                    <UncontrolledTooltip delay={0} target="tooltip184698705">
+                      트위터
+                    </UncontrolledTooltip>
+                  </NavItem>
+                  <NavItem className="d-none d-lg-block ml-lg-4">
+                    <Button
+                      className="btn-neutral btn-icon"
+                      color="default"
+                      href="https://www.creative-tim.com/product/argon-design-system-react?ref=adsr-navbar"
+                      target="_blank"
+                    >
+                      <span className="btn-inner--icon">
+                        <i className="fa fa-cloud-download mr-2" />
+                      </span>
+                      <span className="nav-link-inner--text ml-1">
+                        Download
+                      </span>
+                    </Button>
+                  </NavItem>
+                </Nav>
+              </UncontrolledCollapse>
+            </Container>
+          </Navbar>
+        </header>
+      </>
+    );
+  }
+}
+
+export default DemoNavbar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "assets/vendor/nucleo/css/nucleo.css";
+import "assets/vendor/font-awesome/css/font-awesome.min.css";
+import "assets/scss/argon-design-system-react.scss?v1.1.0";
+
+import Index from "views/Index.js";
+import Landing from "views/examples/Landing.js";
+import Login from "views/examples/Login.js";
+import Profile from "views/examples/Profile.js";
+import Register from "views/examples/Register.js";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Index />} />
+      <Route path="/landing-page" exact element={<Landing />} />
+      <Route path="/login-page" exact element={<Login />} />
+      <Route path="/profile-page" exact element={<Profile />} />
+      <Route path="/register-page" exact element={<Register />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
